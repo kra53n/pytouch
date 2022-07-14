f6d9a9705de9f48710e9d9ae7222aba8b64ecf95
Krai53n <greasha46@gmail.com>
2022-07-14 11:34:52 +0300

    fix sound effects level

--- pytouch/music.py
+++ pytouch/music.py
@@ -7,20 +7,22 @@ from constants import DATA_PATH
 from settings import load_data
 
 
+from pprint import pprint
+
 class Music:
     def __init__(self):
         self.settings = load_data()
         self.sounds = {
             'music': {
-                'music': {'play': True, 'ch': 0, 'snd': 0},
+                'music': {'loop': True, 'ch': 0, 'snd': 0},
             },
             'effects': {
-                'user_reach': {'play': False, 'ch': 1, 'snd': 1},
-                'user_missed': {'play': False, 'ch': 1, 'snd': 2},
+                'user_reach': {'loop': False, 'ch': 1, 'snd': 1},
+                'user_missed': {'loop': False, 'ch': 1, 'snd': 2},
             }
         }
 
-        self.update()
+        self.update(skip=('effects',))
 
     def write_settings(self, data: dict, filename: str):
         if not DATA_PATH.is_dir():
@@ -33,10 +35,11 @@ class Music:
             if user_snd_name == snd_name:
                 px.play(snd_data['ch'], snd_data['snd'])
 
-    def update(self):
-        for music_group in self.sounds.values():
-            for snd_name, snd in music_group.items():
-                if snd_name == 'music':
-                    px.sound(snd['ch']).set_volumes(str(self.settings[snd_name]))
-                    px.play(snd['ch'], snd['snd'], loop=True)
+    def update(self, skip):
+        for music_group, music_group_vals in self.sounds.items():
+            if music_group in skip:
+                continue
+            for snd in music_group_vals.values():
+                px.sound(snd['ch']).set_volumes(str(self.settings[music_group]))
+                px.play(snd['ch'], snd['snd'], loop=snd['loop'])
 
